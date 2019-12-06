2eb85df8612113cc83099f361d54444269cea5a2
Cobo <antoniocogo@gmail.com>
2019-12-06 11:10:13 +0100

    Comment and clean BitReader

--- BinaryStream/BitReader.cs
+++ BinaryStream/BitReader.cs
@@ -7,43 +7,50 @@ using DebugBinaryNumber =
 #endif
 ;
 
-namespace SickDev.BinaryStream {
-    public class BitReader {
+namespace SickDev.BinaryStream
+{
+	public class BitReader
+	{
 		readonly long length;
-        readonly byte[] data;
+		readonly byte[] data;
 		int position;
+		int byteIndex;
+		int bitIndex;
 
 		public long bitsLeft => length - position;
-        public bool canRead => bitsLeft > 0;
+		public bool canRead => bitsLeft > 0;
 
-        public BitReader(byte[] data) {
+		public BitReader(byte[] data)
+		{
 			this.data = data;
 			length = data.LongLength * BinaryNumber.bitsPerByte;
-        }
+		}
 
-        public ulong Read(int bits)
+		public ulong Read(int amountOfBits)
 		{
-			if (position + bits > length)
-				throw new Exception($"Attempting to read {bits} bits, but there's only {bitsLeft} bits left");
+			if (position + amountOfBits > length)
+				throw new Exception($"Attempting to read {amountOfBits} bits, but there's only {bitsLeft} bits left");
 
 			DebugBinaryNumber value = 0;
 
-			int byteIndex = position / BinaryNumber.bitsPerByte;
-			int bitIndex = position % BinaryNumber.bitsPerByte;
-
-			for (int i = 0; i < bits; i++)
+			//For every bit we want to read...
+			for (int i = 0; i < amountOfBits; i++)
 			{
+				//...first, get the byte we are currently reading from...
 				DebugBinaryNumber @byte = data[byteIndex];
 				DebugBinaryNumber mask = MaskUtility.MakeShifted(bitIndex);
-				DebugBinaryNumber maskResult = @byte & mask;
+				//...and then get the appropiate bit from that byte
+				DebugBinaryNumber bit = @byte & mask;
 
-				int bitsToShift = i - bitIndex;
-				if (bitsToShift < 0)
-					maskResult >>= -bitsToShift;
+				//And write that bit into the final value
+				int shiftAmount = i - bitIndex;
+				if (shiftAmount < 0)
+					bit >>= -shiftAmount;
 				else
-					maskResult <<= bitsToShift;
-				value |= maskResult;
+					bit <<= shiftAmount;
+				value |= bit;
 
+				//Update the bit and byte we next have to read from
 				position++;
 				byteIndex = position / BinaryNumber.bitsPerByte;
 				bitIndex = position % BinaryNumber.bitsPerByte;
@@ -51,5 +58,5 @@ namespace SickDev.BinaryStream {
 
 			return value;
 		}
-    }
+	}
 }
