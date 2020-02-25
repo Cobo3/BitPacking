25b18729a4c2e7662c7f673ba18e3e38bee62b17
Cobo <antoniocogo@gmail.com>
2020-02-25 23:27:41 +0100

    Added Nunit

---
+++ Test/BinaryNumberTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace SickDev.BitPacking.Tests
+{
+	[TestFixture]
+	class BinaryNumberTests
+	{
+		[Test]
+		public void ZeroHas1SignificantBit()
+		{
+			BinaryNumber binary = 1;
+			Assert.That(binary.significantBits, Is.EqualTo(1));
+		}
+	}
+}
